814e4331c41b32fdeb7ac9ba498e2b0b6b55ef49
Derefer.Red <43846516+derefer-red@users.noreply.github.com>
2018-10-08 18:07:50 +0200

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -2,12 +2,13 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<meta http-equiv="refresh" content="0; URL=<?php echo $url; ?>" />
+<meta http-equiv="refresh" content="0; URL=<?php echo "https://dereferred.page.link/?link=" . $url; ?>" />
+<!-- Pls. get your own .page.link subdomain at console.firebase.google.com/project/derefer-red/durablelinks -->
 <title><?php echo $url; ?> &hellip;</title>
 </head>
 <body>
 <div align="center">
-  <p><?php echo $url; ?> &hellip;</p>
+  <p><a href="<?php echo $url; ?>"><?php echo $url; ?></a> &hellip;</p>
 </div>			
 </body>
 </html>
